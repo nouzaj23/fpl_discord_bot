bae0a689a535acf31d0309e9140fef9ae8d91b88
Jan Nouza <nouzaj23@gmail.com>
2024-02-01 15:23:56 +0100

    Add fetching right after startup

--- fetching/fetchHandler.go
+++ fetching/fetchHandler.go
@@ -7,6 +7,7 @@ import (
 )
 
 func HandleFetch(pr repository.PlayerRepository, tr repository.TeamRepository, s *discordgo.Session) {
+	FetchAndUpdate(pr, tr, s)
 	ticker := time.NewTicker(time.Minute * 15)
 	defer ticker.Stop()
 	for {
